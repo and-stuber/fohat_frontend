403932116f9b16bc37b52b3ed078e92906d811cc
Anderson <andersoni.trybe@gmail.com>
2020-11-11 15:05:20 -0300

    Button play again, redirect to Login

---
+++ src/components/BtnPlayAgain.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const BtnPlayAgain = () => {
+
+  return(
+    <Link to="/" className="btn" >
+    Play again!
+    </Link>
+  )
+}
+
+export default BtnPlayAgain;
