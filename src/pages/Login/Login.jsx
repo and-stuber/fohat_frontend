0c0256d7d25cafc96600ef470c9b2a78c6b5a838
Anderson <andersoni.trybe@gmail.com>
2020-11-10 23:42:04 -0300

    Login page call for LoginForm component

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import LoginForm from '../../components/LoginForm';
 import './Login.css';
 
 const Login = () => {
   return (
     <div>
     Login page
+    <LoginForm />
     </div>
   )
 }
