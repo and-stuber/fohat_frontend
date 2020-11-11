5b1cbdfbc3dd1100797bf3548996226beace9ea1
Anderson <andersoni.trybe@gmail.com>
2020-11-10 22:40:17 -0300

    Upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import './App.css';
+
+export default function App() {
+  return (
+    <Switch>
+      <Route path="/" />
+    </Switch>
+  );
+}
