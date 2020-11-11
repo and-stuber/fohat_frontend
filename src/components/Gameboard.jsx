60075cd14bf68e04037f07244b9fe7915e5c1de3
Anderson <andersoni.trybe@gmail.com>
2020-11-11 10:09:09 -0300

    Gameboard basic structure

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 class Gameboard extends React.Component {
   render() {
     return(
-      <div>
+      <div className="game-container">
+        <div className="question">
+          <p>Timer</p>
+          <div className="question-category">Category: </div>
+          <div className="question-text">Question text</div>
+        </div>
+        <div className="answers">
+          Buttons for answers
+        </div>
       </div>
     )
   }
