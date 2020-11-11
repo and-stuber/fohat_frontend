d64bcabac09e51ed6592a44951e5d46ab6adeea8
Anderson <andersoni.trybe@gmail.com>
2020-11-10 23:15:26 -0300

    App routes for login and game page.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Login from './pages/Login';
+import Game from './pages/Game';
 
 export default function App() {
   return (
     <Switch>
-      <Route path="/" />
+      <Route path="/game" component={ Game } />
+      <Route exact path="/" component={ Login } />
     </Switch>
   );
 }
