90026dfa71eca3c0d5c0509c9f9ff9e657e112a6
Anderson <andersoni.trybe@gmail.com>
2020-11-11 10:15:22 -0300

    Timer functions prebuild

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 
 class Gameboard extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      timer: 30,
+    }
+  }
+
+  timerFunction() {
+    const { timer, stop } = this.state;
+    if (timer > 0 && !stop) {
+      this.setState((prevState) => ({
+        timer: prevState.timer - 1,
+      }));
+    } else {
+      this.setState({ stop: true });
+    }
+  }
+
   render() {
+    const { timer } = this.state;
     return(
       <div className="game-container">
         <div className="question">
-          <p>Timer</p>
+          <span className="timer">
+            {timer > 0 ? `Time: ${timer}` : `Time's Up`}
+          </span>
           <div className="question-category">Category: </div>
           <div className="question-text">Question text</div>
         </div>
