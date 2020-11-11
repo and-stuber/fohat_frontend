9e7f0e370fc68dbfc13f897bd2d2087284becfe8
Anderson <andersoni.trybe@gmail.com>
2020-11-11 12:18:55 -0300

    Routes and Prebuild for Scoreboard

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
 import Game from './pages/Game';
+import Scoreboard from './pages/Scoreboard/Scoreboard';
 
 export default function App() {
   return (
     <Switch>
+      <Route path="/scoreboard" component={ Scoreboard } />
       <Route path="/game" component={ Game } />
       <Route exact path="/" component={ Login } />
     </Switch>
