7d3e8cccaab79ee8ae5ee8fa8dbd6f95626a13ce
Anderson <andersoni.trybe@gmail.com>
2020-11-11 09:42:25 -0300

    Lint fix for comma

--- src/services/triviaAPI.js
+++ src/services/triviaAPI.js
@@ -1,7 +1,7 @@
 const API_URL = "https://opentdb.com/api.php?amount=5&token="
 
 const triviaAPI = (token) => (
-  fetch(`${API_URL, token}`)
+  fetch(`${API_URL}${token}`)
     .then((response) => response.json())
 );
 
