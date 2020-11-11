f59d250a4ad94ef529af839c7f2b61c5de18c5d2
Anderson <andersoni.trybe@gmail.com>
2020-11-11 12:27:48 -0300

    Routes for correct redirect

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import './App.css';
-import Login from './pages/Login';
-import Game from './pages/Game';
-import Scoreboard from './pages/Scoreboard/Scoreboard';
+import Routes from './components/Routes';
 
 export default function App() {
   return (
-    <Switch>
-      <Route path="/scoreboard" component={ Scoreboard } />
-      <Route path="/game" component={ Game } />
-      <Route exact path="/" component={ Login } />
-    </Switch>
+    <Routes />
   );
 }
